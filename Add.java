924247e9f6fca234d1d155a19280e1b84fadd1dc
DIPESH29 <77069615+DIPESH29@users.noreply.github.com>
2021-09-03 20:00:32 +0530

    first commit for add

---
+++ Add.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+ class Add{
+
+public static void main(String[] args)
+
+{
+int a,b;
+float c;
+Scanner s=new Scanner(System.in);
+System.out.println("Enter two numbers");
+ a=s.nextInt();
+ b=s.nextInt();
+ c=a/b;
+System.out.println("Sum is " +c);
+
+}
+}
